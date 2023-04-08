b397d9075e8e6d50db5fb78ff0933fb707f7a4b4
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-04-09 01:01:42 +0530

    Inital Setup

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface ButtonProps {
+    
+}
+ 
+const Button: FC<ButtonProps> = () => {
+    return (
+        <div>
+            Button
+        </div>
+    );
+}
+ 
+export default Button;
