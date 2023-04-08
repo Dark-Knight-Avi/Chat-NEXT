fea660dd77c79a6bce854867e0f972e17ec3090a
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-04-09 02:09:25 +0530

    Added: Button variants utility for diferent variants and size

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
+import Button from "@/components/ui/Button";
 import { FC } from "react";
 
-interface pageProps {
-    
-}
- 
+interface pageProps {}
+
 const page: FC<pageProps> = () => {
-    return (
-        <div>page</div>
-    );
-}
- 
+  return <Button>hello</Button>;
+};
+
 export default page;
---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { Input } from "postcss";
+import { clsx, ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export default cn;
