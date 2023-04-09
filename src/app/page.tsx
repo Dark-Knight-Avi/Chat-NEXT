f346dbc85949a51f00eeccc9af85f7837b7302fa
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-04-09 15:20:48 +0530

    Removed the testing line of db connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db"
 
 export default async function Home() {
-  await db.set('Name', 'Aritra Karmakar')
   return (
     <div className="text-red-500">hello world</div>
   )
