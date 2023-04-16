89b29a85302f1bee5c8a1053bf70b7aa5b07f3ef
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-04-16 18:25:34 +0530

    Updated Cache Behaviour of Next JS

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions, User } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { db } from "./db";
+import { fetchRedis } from "@/helpers/redis";
 
 const getGoogleCredentials = () => {
   const clientID = process.env.GOOGLE_CLIENT_ID;
@@ -32,12 +33,15 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user }) {
-      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
-
-      if (!dbUser) {
+      // const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+      const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
+        | string
+        | null;
+      if (!dbUserResult) {
         token.id = user!.id;
         return token;
       }
+      const dbUser = JSON.parse(dbUserResult) as User;
       return {
         id: dbUser.id,
         name: dbUser.name,
